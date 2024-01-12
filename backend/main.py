aa109baa42891bfdfc210e48f3d4533e884675f1
arcstep <43801@qq.com>
2024-01-12 17:41:12 +0800

    Add CORS middleware to allow requests from localhost:3000

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langserve import add_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 # 加载 .env 到环境变量
 from dotenv import load_dotenv, find_dotenv
@@ -11,6 +12,19 @@ app = FastAPI(
     title="LangServe Server",
     version="1.0",
     description="Wencheng Agent API server",
+)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # 添加OpenAI聊天模型的路由
@@ -23,9 +37,9 @@ async def get_chat_history(user_id: str):
     # 根据user_id查询聊天历史记录的逻辑
     # 返回聊天历史记录
     return {"user_id": user_id, "history": [
-        {"user_id": user_id, "text": "Hello, world!", "timestamp": "2021-01-01 00:00:00"},
-        {"user_id": user_id, "text": "Hello, world!", "timestamp": "2021-01-01 00:00:00"},
-        {"user_id": user_id, "text": "Hello, world!", "timestamp": "2021-01-01 00:00:00"},    
+        {"seq":1, "user_id": user_id, "text": "你好!", "timestamp": "2021-01-01 00:00:00"},
+        {"seq":2, "user_id": "文成公主", "text": "你也好!", "timestamp": "2021-01-01 00:00:01"},
+        {"seq":3, "user_id": user_id, "text": "我们聊聊!", "timestamp": "2021-01-01 00:00:02"},    
     ]}
 
 if __name__ == "__main__":
