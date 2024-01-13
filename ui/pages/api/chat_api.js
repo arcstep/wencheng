37d59f4c3b620618f1313edba360c3abeca5847e
arcstep <43801@qq.com>
2024-01-13 10:43:51 +0800

    Add langchain dependency and update chat API endpoint
    Integrate langchain for remote execution of chat messages

--- ui/pages/api/chat_api.js
+++ ui/pages/api/chat_api.js
@@ -6,9 +6,9 @@ const base_url = 'http://localhost:8000'
 
 export default async function chat_history() {
   try {
-    const response = await axios.get(`${base_url}/chat_history/myuserid`);
+    const response = await axios.get(`${base_url}/chat_history/1`);
     console.log("RESPONSE:", response.data)
-    return response.data.history;
+    return response.data;
   } catch (error) {
     console.error("ERROR:", error);
   }
--- ui/pages/chat.js
+++ ui/pages/chat.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import chat_history from './api/chat_api';
+import {RemoteRunnable} from "langchain/runnables/remote";
 
 const ChatPage = () => {
   const [chatHistory, setChatHistory] = useState([]);  
@@ -16,24 +17,46 @@ const ChatPage = () => {
         console.error(error);
       }
     };
-
     fetchChatHistory();
+    console.log("Chat history:", chatHistory)
   }, []);
   
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     // 在这里发送你的消息
-    console.log(currentMessage);
+    const newMessage = currentMessage;
+    console.log(newMessage);
     // 将新的消息添加到聊天记录中
     setChatHistory([...chatHistory, currentMessage]);
     setCurrentMessage('');
+    const chain = new RemoteRunnable({
+      url: `http://localhost:8000/chat/`,
+    });
+    try {
+      const reply = await chain.invoke(
+        { 'human_input': newMessage },
+        { 'configurable': { 'session_id': "1" } }
+      );
+      console.log(reply);
+      const msg = {
+        'type': reply.type,
+        'content': reply.content    
+      };
+      setChatHistory(chatHistory => [...chatHistory, msg]);
+    } catch (error) {
+      if (error.name === 'AbortError') {
+        console.log('Request was aborted');
+      } else {
+        throw error;
+      }
+    }
   };
 
   return (
     <Layout title="我们随便聊聊">
       <ul>
-        {chatHistory.map(({ seq, user_id, text, timestamp }) => (
-          <li key={seq}>{ user_id }({timestamp}): {text}</li>
+        {chatHistory.map(({ type, content }, index) => (
+          <li key = {index}>{ type }: {content}</li>
         ))}
       </ul>
       <form onSubmit={handleSubmit}>
