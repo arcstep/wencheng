37a1f7ca059b23b9a3092540883341fffe64613a
arcstep <43801@qq.com>
2024-03-08 10:23:42 +0800

    Update langchain-chinese version and refactor memory management

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI, HTTPException
 from langchain.chains import LLMChain
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema.output_parser import StrOutputParser
-from langchain.memory import ConversationBufferWindowMemory
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.runnables import (
     ConfigurableField,
     ConfigurableFieldSpec,
@@ -22,16 +18,18 @@ from langfuse.callback import CallbackHandler
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import AsyncIterator, Any
-from .chat_history import create_new_chat
 
-# 存储对话历史
-memoryStore = {}
+# 记忆管理
+from langchain_chinese import MemoryManager
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
+from langchain_chinese import WithMemoryBinding
 
-# 记忆提取和追加
-def get_memory(session_id: str) -> BaseChatMessageHistory:
-    if session_id not in memoryStore:
-        memoryStore[session_id] = ChatMessageHistory()
-    return memoryStore[session_id]
+window = ConversationBufferWindowMemory(
+  return_messages=True, k=20, chat_memory = ChatMessageHistory()
+)
+
+memory = MemoryManager(shorterm_memory = window)
 
 # 设置调试模式
 set_debug(False)
@@ -78,13 +76,12 @@ _handler = CallbackHandler(trace_name="chat-agent", user_id="wencheng")
 
 # 构造智能体
 def create_chat_agent(llm, prompt = _prompt, handler = _handler):
-    chain = RunnableWithMessageHistory(
+    chain = WithMemoryBinding(
         prompt | llm | StrOutputParser(),
-        get_memory,
+        memory,
         input_messages_key="question",
         history_messages_key="history",
     ).with_config({"callbacks": [handler]})
-    
     return chain
 
 #####################################
@@ -104,13 +101,13 @@ add_routes(
 
 def create_zhipu_translater():
     llm = ChatZhipuAI(model="glm-4", temperature=0.01)
-    chain = _prompt_translater | llm | StrOutputParser()
-    return RunnableWithMessageHistory(
-        chain,
-        lambda session_id: ChatMessageHistory(),
+    chain = WithMemoryBinding(
+        _prompt_translater | llm | StrOutputParser(),
+        memory,
         input_messages_key="question",
         history_messages_key="history",
     ).with_config({"callbacks": [_handler]})
+    return chain
 
 add_routes(
     app, 
@@ -145,16 +142,18 @@ add_routes(
     path = "/agent/gpt4")
 
 #####################################
-
-# 查询聊天历史记录的接口
-@app.get("/chat_history/{session_id}")
-async def chat_history(session_id: str):
-    return get_chat_history_by_session_id(session_id)
+def create_session_id():
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y%m%d-%H%M%S")
+    random_number = random.randint(1000, 9999)
+    # 组合成 session id
+    session_id = f"{date_time}-{random_number}"
+    return session_id
 
 # 创建新的对话轮次
-@app.post("/chat_history")
+@app.post("/session")
 async def chat_create():
-    return create_new_chat()
+    return create_session_id()
 
 # 列举可用的智能体清单
 @app.get("/agents")
